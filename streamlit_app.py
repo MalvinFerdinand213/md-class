3baaa7f6481f81b2f8c0c2f8cff96b3dafe7e23d
MalvinFerdinand213 <74392043+MalvinFerdinand213@users.noreply.github.com>
2025-03-17 14:16:15 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,9 @@ def main():
   st.title('Dermatology Machine Learning')
   st.info('This App Will Use Machine Learning')
 
+  #input data by user
+  erythema = st.slider('Erythema', min_value=0, max_value=3, value=2)
+
+
 if __name__ == "__main__":
   main()
